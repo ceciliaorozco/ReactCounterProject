e6e3359a6cedb2e8c0b3ca4b05ff04947cd867a2
CeciliaOrozcoArriaga <corozco@icstars.org>
2023-10-05 11:08:20 -0500

    reactcounterproject

---
+++ counter/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import'./App.css';
+import Counter from './Counter';
+
+function App () {
+  return (
+    <div className="App">
+      <h1>Simple React Counter</h1>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
